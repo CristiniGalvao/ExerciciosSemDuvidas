3bb37a8f6485d0f15052abed0cafe2dd0a969241
Gabriella Cristini <126834537+CristiniGalvao@users.noreply.github.com>
2023-03-18 20:40:11 -0300

    Add

---
+++ Bike/src/main/java/com/mycompany/construtorbike/CadastrarBike.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.construtorbike;
+
+/**
+ *Criar uma Classe Bicicleta com 3 Atributos públicos e 4 Atributos privados; 
+ * criar 2 métodos para cadastrar os atributos privados; Criar um construtor para atribuir 2 atributos; 
+ * Na Classes principal Cadastrar no mínimo 4 Bicicletas;
+ * Na Classe principal Mostrar 4 Mensagens destacando os 7 atributos das 2 primeiras bicicletas cadastradas;
+ */
+public class CadastrarBike {
+    
+}
---
+++ Bike/src/main/java/com/mycompany/construtorbike/ConstrutorBike.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.construtorbike;
+
+/**
+ *
+ * @author Usuario
+ */
+public class ConstrutorBike {
+
+    public static void main(String[] args) {
+       
+    }
+}
